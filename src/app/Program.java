5f5de16955359ca8d0b11dee05977931d0749db3
Lucas Camargo <lucascamargo005@gmail.com>
2026-01-13 16:48:10 -0300

    Adicionada a classe Seller

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,13 +1,9 @@
 package app;
 
-import db.DB;
-import db.dbException;
-import db.dbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
 
@@ -15,7 +11,10 @@ public class Program {
 
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(),
+                3000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
